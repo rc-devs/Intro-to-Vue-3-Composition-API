c93f1df81898aed7d97abf17cb0c4b9be7f67476
rc-devs <rc.devs9@gmail.com>
2025-09-25 13:16:19 -0500

    v-if/else  and inStock variable

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
-
+const inStock = ref(true) 
 </script>
   
 <template>
@@ -16,6 +16,8 @@ const image = ref(socksGreenImage)
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else="inStock">Out of Stock</p>
       </div>
     </div>
   </div>
