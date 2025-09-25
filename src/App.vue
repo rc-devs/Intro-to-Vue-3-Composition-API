9f18cd2fd165a7261459a795152aa6ff5cd7e20d
rc-devs <rc.devs9@gmail.com>
2025-09-25 12:04:28 -0500

    code challenge; add description variable and display in p tag

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 
 const product = ref('Socks')
+const description = ref ('These socks are a blend of cotton and polyester')
 </script>
 
 
@@ -10,6 +11,7 @@ const product = ref('Socks')
     <div class="product-container">
       <div class="product-info">
         <h1>{{product}}</h1>
+        <p>{{description}}</p>
       </div>
     </div>
   </div>
