6eeebe6d9704c170bfcf93f1010eb133f97cd2ec
rc-devs <rc.devs9@gmail.com>
2025-09-25 11:58:42 -0500

    add ref variable 'products' and string interpolate for dynamic display

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const product = ref('Socks')
+</script>
+
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{product}}</h1>
       </div>
     </div>
   </div>
