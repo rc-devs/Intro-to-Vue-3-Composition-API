00cc8e371ae94b40769f8db9f01866c26e4e6c4a
rc-devs <rc.devs9@gmail.com>
2025-09-25 14:04:21 -0500

    update image.value to conditionally change image(color) on mouseover

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import socksGreenImage from './assets/images/socks_green.jpeg'
+import socksBlueImage from './assets/images/socks_blue.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
@@ -9,13 +10,15 @@ const inStock = true
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
 const variants = ref([
-  { id: 2234, color: 'green' },
-  { id: 2235, color: 'blue' },
+  { id: 2234, color: 'green', image: socksGreenImage },
+  { id: 2235, color: 'blue' , image: socksBlueImage},
 ])
 
 const cart = ref(0)
 
 const addToCart = () => cart.value += 1
+
+const updateImage = (variantImage) => {image.value = variantImage} // pass variant image and set image value to variant image
 </script>
   
 <template>
@@ -36,6 +39,7 @@ const addToCart = () => cart.value += 1
         <div
           v-for="variant in variants"
           :key="variant.id"
+          @mouseover="updateImage(variant.image)"
         >
           {{ variant.color }}
         </div>
